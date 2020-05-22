69189f34d3e2506a4086bc1032065e261b4d3950
MOSvisio <lucas.schutz@vivoka.com>
2020-05-22 16:21:58 +0200

    favories

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,14 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
   offres: Observable<any>;
+  personne: any;
+  id: number = 1;
 
-  constructor(public httpClient: HttpClient) { }
+  constructor(public httpClient: HttpClient) {
+    this.getPersonne(this.id).subscribe(data => {
+      this.personne = data;
+    })
+   }
 
   getAllOffres() {
     return this.httpClient.get("https://damp-scrubland-46949.herokuapp.com/emplois");
@@ -21,7 +27,27 @@ export class ApiService {
   }
 
   getFavorite(id) {
-    return this.httpClient.get("https://damp-scrubland-46949.herokuapp.com/favorises/" + id);
+    let idFavoris;
+    return this.httpClient.get("https://damp-scrubland-46949.herokuapp.com/favorises/" + this.personne['favoris']['id'])
+  }
+
+  getPersonne(id) {
+    return this.httpClient.get("https://damp-scrubland-46949.herokuapp.com/personnes/" + id);
+  }
+
+  addFavoriteEmploiId(id) {
+    let dataArray  = {
+      "emplois" : []
+    }
+    this.getFavorite(0).subscribe(data => {
+
+      let emplois = data["emplois"]
+      for (var offre of emplois) {
+        dataArray["emplois"].push(offre['id'])
+      }
+      dataArray["emplois"].push(id)
+      this.httpClient.put("https://damp-scrubland-46949.herokuapp.com/favorises/" + this.personne['favoris']['id'], dataArray);
+    })
   }
 
 }
